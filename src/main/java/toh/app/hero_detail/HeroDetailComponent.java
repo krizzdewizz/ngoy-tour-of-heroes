0d75e85a9139de33bb166f1b2e07634bddf9d92d
Christian Oetterli <christian.oetterli@bison-group.com>
2018-11-28 03:40:27 +0100

    save hero, error/info messages

--- src/main/java/toh/app/hero_detail/HeroDetailComponent.java
+++ src/main/java/toh/app/hero_detail/HeroDetailComponent.java
@@ -1,14 +1,24 @@
 package toh.app.hero_detail;
 
+import static java.lang.String.format;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import ngoy.core.Component;
 import ngoy.core.Inject;
+import ngoy.core.OnDestroy;
 import ngoy.core.OnInit;
+import ngoy.router.Location;
 import ngoy.router.RouteParams;
 import toh.app.Hero;
 import toh.app.services.HeroService;
 
 @Component(selector = "app-hero-detail", templateUrl = "hero-detail.component.html", styleUrls = { "hero-detail.component.css" })
-public class HeroDetailComponent implements OnInit {
+@Controller
+public class HeroDetailComponent implements OnInit, OnDestroy {
 	public Hero hero;
 
 	@Inject
@@ -17,10 +27,41 @@ public class HeroDetailComponent implements OnInit {
 	@Inject
 	public HeroService heroService;
 
+	@Inject
+	public Location location;
+
+	public boolean saved;
+	public String error;
+
+	private String redirectUrl;
+
 	@Override
 	public void ngOnInit() {
 		hero = heroService.findById(Long.parseLong(routeParams.get("id")))
-				.get();
+				.orElse(null);
+		redirectUrl = location.getPath();
 	}
 
+	@Override
+	public void ngOnDestroy() {
+		saved = false;
+		error = null;
+	}
+
+	@PostMapping("/detail/savehero")
+	public String saveHero(Hero heroRequest) {
+		String name = heroRequest.name;
+		if (name == null || name.isEmpty()) {
+			error = "Name must not be empty.";
+		} else {
+			Optional<Hero> hero = heroService.findById(heroRequest.id);
+			if (hero.isPresent()) {
+				hero.get().name = name;
+				saved = true;
+			} else {
+				error = format("hero with id %s could not be found.", heroRequest.id);
+			}
+		}
+		return format("redirect:%s", redirectUrl);
+	}
 }
